9509c3238474de4755c892b1830a56397de6f6ee
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-11-09 23:08:03 +0100

    fixup! Move SAML configurations to a table of their own

--- lib/Command/GetMetadata.php
+++ lib/Command/GetMetadata.php
@@ -26,6 +26,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use OCA\User_SAML\SAMLSettings;
+use OneLogin\Saml2\Error;
 use OneLogin\Saml2\Settings;
 
 class GetMetadata extends Command {
